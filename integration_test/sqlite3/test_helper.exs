e6a41c9bd8f4c594d9e6a70a8e8b29f4ee428522
Kevin Lang <me@gnalck.com>
2020-05-12 21:12:44 -0700

    add failing tests for basic sql stuff

--- integration_test/sqlite3/test_helper.exs
+++ integration_test/sqlite3/test_helper.exs
@@ -1,2 +1,55 @@
 Logger.configure(level: :info)
+
+# configure
+Application.put_env(:ecto, :primary_key_type, :id)
+Application.put_env(:ecto, :async_integration_tests, false)
+# TODO? lock_for_update
+
+# add some extra telemetry
+Code.require_file("../support/repo.exs", __DIR__)
+
+alias Ecto.Integration.TestRepo
+
+Application.put_env(
+  :ecto_sql,
+  TestRepo,
+  path: "test.db"
+)
+
+defmodule Ecto.Integration.TestRepo do
+  use Ecto.Integration.Repo, otp_app: :ecto_sql, adapter: Ecto.Adapters.SQLite3
+
+  def create_prefix(prefix) do
+    raise "todo"
+  end
+
+  def drop_prefix(prefix) do
+    raise "todo"
+  end
+
+  def uuid do
+    Ecto.UUID
+  end
+end
+
+# Load support files
+ecto = Mix.Project.deps_paths()[:ecto]
+Code.require_file("#{ecto}/integration_test/support/schemas.exs", __DIR__)
+# Code.require_file("../support/migration.exs", __DIR__)
+
+defmodule Ecto.Integration.Case do
+  use ExUnit.CaseTemplate
+end
+
+{:ok, _} = Ecto.Adapters.SQLite3.ensure_all_started(TestRepo.config(), :temporary)
+
+_ = Ecto.Adapters.SQLite3.storage_down(TestRepo.config())
+:ok = Ecto.Adapters.SQLite3.storage_up(TestRepo.config())
+
+{:ok, _pid} = TestRepo.start_link()
+# :ok = Ecto.Migrator.up(TestRepo, 0, Ecto.Integration.Migration, log: :debug)
+# Ecto.Adapters.SQL.Sandbox.mode(TestRepo, :manual)
+
+Process.flag(:trap_exit, true)
+
 ExUnit.start()
--- lib/ecto/adapters/sqlite3/connection.ex
+++ lib/ecto/adapters/sqlite3/connection.ex
@@ -1,4 +1,15 @@
 if Code.ensure_loaded(XQLite3) do
   defmodule Ecto.Adapters.SQLite3.Connection do
+    @behaviour Ecto.Adapters.SQL.Connection
+
+    @impl Ecto.Adapters.SQL.Connection
+    def child_spec(opts) do
+      XQLite3.child_spec(opts)
+    end
+
+    @impl Ecto.Adapters.SQL.Connection
+    def query(conn, sql, params, opts) do
+      XQLite3.query(conn, sql, params, opts)
+    end
   end
 end
