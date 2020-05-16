6d283df247584cb3fff551351790cfd059b7f2a8
Kevin Lang <me@gnalck.com>
2020-05-15 20:52:57 -0700

    successfully submitting query and getting result for migration

--- lib/ecto/adapters/sqlite3/connection.ex
+++ lib/ecto/adapters/sqlite3/connection.ex
@@ -14,7 +14,7 @@ if Code.ensure_loaded(XQLite3) do
 
     @impl true
     def prepare_execute(conn, name, sql, params, opts) do
-      SQLite3.prepare_execute(conn, name, sql, params, opts)
+      XQLite3.prepare_execute(conn, name, sql, params, opts)
     end
 
     @impl true
@@ -35,6 +35,34 @@ if Code.ensure_loaded(XQLite3) do
     #   IO.puts(inspect(_returning))
     #   raise ArgumentError, ":returning is not supported in insert/insert_all by SQLite3"
     # end
+
+    alias Ecto.Query.{BooleanExpr, JoinExpr, QueryExpr, WithExpr, Tagged}
+
+    @impl true
+    def all(query, as_prefix \\ []) do
+      IO.inspect(query)
+      sources = create_names(query, as_prefix)
+
+      cte = [] #cte(query, sources)
+      from = from(query, sources)
+      IO.inspect(from)
+      select = select(query, sources)
+      IO.inspect(select)
+      join = [] #join(query, sources)
+      where = [] #where(query, sources)
+      group_by = [] #group_by(query, sources)
+      having = [] #having(query, sources)
+      window = [] #window(query, sources)
+      combinations = [] #combinations(query)
+      order_by = [] #order_by(query, sources)
+      limit = [] #limit(query, sources)
+      offset = [] #offset(query, sources)
+      lock = [] #lock(query, sources)
+
+      [cte, select, from, join, where, group_by, having, window, combinations, order_by, limit, offset | lock]
+    end
+
+    # Migrations
 
     alias Ecto.Migration.{Table, Index, Reference, Constraint}
 
@@ -100,6 +128,8 @@ if Code.ensure_loaded(XQLite3) do
       {"SELECT true FROM sqlite_master where type = 'table' and name = $1", [table]}
     end
 
+    # impl
+
     defp insert_all(rows) do
       intersperse_map(rows, ?,, fn row ->
         [?(, intersperse_map(row, ?,, &insert_all_value/1), ?)]
@@ -131,6 +161,10 @@ if Code.ensure_loaded(XQLite3) do
       if condition, do: value, else: []
     end
 
+    defp escape_string(value) when is_binary(value) do
+      value |> :binary.replace("'", "''", [:global])
+    end
+
     defp column_definitions(table, columns) do
       intersperse_map(columns, ", ", &column_definition(table, &1))
     end
@@ -149,9 +183,6 @@ if Code.ensure_loaded(XQLite3) do
       [quote_name(name), ?\s, column_type(type, opts), column_options(type, opts)]
     end
 
-    # todo: handle datetimes better
-    defp column_type(atom, _query), do: Atom.to_string(atom)
-
     defp column_options(type, opts) do
       default = Keyword.fetch(opts, :default)
       null = Keyword.get(opts, :null)
@@ -163,5 +194,131 @@ if Code.ensure_loaded(XQLite3) do
     defp null_expr(false), do: " NOT NULL"
     defp null_expr(true), do: " NULL"
     defp null_expr(_), do: []
+
+    defp error!(nil, message) do
+      raise ArgumentError, message
+    end
+
+    defp error!(query, message) do
+      raise Ecto.QueryError, query: query, message: message
+    end
+
+    defp create_names(%{sources: sources}, as_prefix) do
+      create_names(sources, 0, tuple_size(sources), as_prefix) |> List.to_tuple()
+    end
+
+    defp create_names(sources, pos, limit, as_prefix) when pos < limit do
+      [create_name(sources, pos, as_prefix) | create_names(sources, pos + 1, limit, as_prefix)]
+    end
+
+    defp create_names(_sources, pos, pos, _as_prefix) do
+      []
+    end
+
+    defp create_name(sources, pos, as_prefix) do
+      case elem(sources, pos) do
+        {:fragment, _, _} ->
+          {nil, as_prefix ++ [?f | Integer.to_string(pos)], nil}
+
+        {table, schema, prefix} ->
+          name = as_prefix ++ [create_alias(table) | Integer.to_string(pos)]
+          {quote_table(prefix, table), name, schema}
+
+        %Ecto.SubQuery{} ->
+          {nil, as_prefix ++ [?s | Integer.to_string(pos)], nil}
+      end
+    end
+
+    defp create_alias(<<first, _rest::binary>>) when first in ?a..?z when first in ?A..?Z do
+      first
+    end
+
+    defp create_alias(_) do
+      ?t
+    end
+
+    defp from(%{from: %{hints: [_ | _]}} = query, _sources) do
+      error!(query, "table hints are not supported by SQLite3")
+    end
+
+    defp from(%{from: %{source: source}} = query, sources) do
+      {from, name} = get_source(query, sources, 0, source)
+      [" FROM ", from, " AS " | name]
+    end
+
+    defp get_source(query, sources, ix, source) do
+      {expr, name, _schema} = elem(sources, ix)
+      {expr || expr(source, sources, query), name}
+    end
+
+    defp select(%{select: %{fields: fields}, distinct: distinct} = query, sources) do
+      [
+        "SELECT ",
+        distinct(distinct, sources, query),
+        select(fields, sources, query)
+      ]
+    end
+
+    defp select([], _sources, _query), do: "TRUE"
+
+    defp select(fields, sources, query) do
+      intersperse_map(fields, ", ", fn
+        {:&, _, [idx]} ->
+          case elem(sources, idx) do
+            {source, _, nil} ->
+              error!(query, "MySQL does not support selecting all fields from #{source} without a schema. " <>
+                            "Please specify a schema or specify exactly which fields you want to select")
+            {_, source, _} ->
+              source
+          end
+        {key, value} ->
+          [expr(value, sources, query), " AS ", quote_name(key)]
+        value ->
+          expr(value, sources, query)
+      end)
+    end
+
+    defp distinct(nil, _sources, _query), do: []
+    defp distinct(%QueryExpr{expr: true}, _sources, _query),  do: "DISTINCT "
+    defp distinct(%QueryExpr{expr: false}, _sources, _query), do: []
+
+    defp distinct(%QueryExpr{expr: exprs}, _sources, query) when is_list(exprs) do
+      error!(query, "DISTINCT with multiple columns is not supported by SQLite3")
+    end
+
+    defp expr({{:., _, [{:&, _, [idx]}, field]}, _, []}, sources, _query) when is_atom(field) do
+      quote_qualified_name(field, sources, idx)
+    end
+
+    defp expr(%Tagged{value: other, type: type}, sources, query) do
+      ["CAST(", expr(other, sources, query), " AS ", ecto_to_db(type, query), ?)]
+    end
+
+    defp expr(nil, _sources, _query),   do: "NULL"
+    defp expr(true, _sources, _query),  do: "TRUE"
+    defp expr(false, _sources, _query), do: "FALSE"
+
+    defp expr(literal, _sources, _query) when is_binary(literal) do
+      [?', escape_string(literal), ?']
+    end
+
+    defp expr(literal, _sources, _query) when is_integer(literal) do
+      Integer.to_string(literal)
+    end
+
+    defp expr(literal, _sources, _query) when is_float(literal) do
+      Float.to_string(literal)
+    end
+
+    defp quote_qualified_name(name, sources, ix) do
+      {_, source, _} = elem(sources, ix)
+      [source, ?. | quote_name(name)]
+    end
+
+    defp column_type(atom, query), do: ecto_to_db(atom, query)
+
+    # todo: better handling of casts/types, datetimes, etc
+    defp ecto_to_db(type, _query), do: Atom.to_string(type)
+
   end
 end
