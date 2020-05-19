796aa27ea9983d1790c49d6c60bc8521e436d937
Kevin Lang <me@gnalck.com>
2020-05-18 18:25:52 -0700

    'override' default insert behavior to account for fact that xqlite does not return updated rows on inserts

--- lib/ecto/adapters/sqlite3.ex
+++ lib/ecto/adapters/sqlite3.ex
@@ -54,5 +54,38 @@ defmodule Ecto.Adapters.SQLite3 do
     false
   end
 
+  @impl true
+  def insert(adapter_meta, schema_meta, params, on_conflict, returning, opts) do
+    %{source: source, prefix: prefix} = schema_meta
+    {_, query_params, _} = on_conflict
+
+    key = primary_key!(schema_meta, returning)
+    {fields, values} = :lists.unzip(params)
+    sql = @conn.insert(prefix, source, fields, [fields], on_conflict, [])
+    opts = [{:cache_statement, "ecto_insert_#{source}"} | opts]
+
+    case Ecto.Adapters.SQL.query(adapter_meta, sql, values ++ query_params, opts) do
+      {:ok, %{num_rows: 1, last_insert_id: last_insert_id}} ->
+        {:ok, last_insert_id(key, last_insert_id)}
+
+      {:error, err} ->
+        case @conn.to_constraints(err, source: source) do
+          []          -> raise err
+          constraints -> {:invalid, constraints}
+        end
+    end
+  end
+
+  defp primary_key!(%{autogenerate_id: {_, key, _type}}, [key]), do: key
+  defp primary_key!(_, []), do: nil
+  defp primary_key!(%{schema: schema}, returning) do
+    raise ArgumentError, "SQLite3 does not support :read_after_writes in schemas for non-primary keys. " <>
+                         "The following fields in #{inspect schema} are tagged as such: #{inspect returning}"
+  end
+
+  defp last_insert_id(nil, _last_insert_id), do: []
+  defp last_insert_id(_key, 0), do: []
+  defp last_insert_id(key, last_insert_id), do: [{key, last_insert_id}]
+
 
 end
